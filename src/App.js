62d5f8906c6b012593596713a9cae20c3eb7548a
myungkeun <audrms3568@gmail.com>
2023-04-10 23:21:54 +0900

    react-practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [글제목, b] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "자바스크립트 독학",
+  ]);
+  let [따봉, 따봉변경] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>My diary</h4>
+      </div>
+      <div className="list">
+        <h5>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(따봉++);
+            }}
+          >
+            🔥
+          </span>
+          {따봉}
+        </h5>
+        <p>2월 27일 발행</p>
+      </div>
+      <div className="list">
+        <h5>
+          {글제목[1]}
+          <span
+            onClick={() => {
+              따봉변경(따봉++);
+            }}
+          >
+            🔥
+          </span>
+          {따봉}
+        </h5>
+        <p>2월 27일 발행</p>
+      </div>
+      <div className="list">
+        <h5>
+          {글제목[2]}
+          <span
+            onClick={() => {
+              따봉변경(따봉++);
+            }}
+          >
+            🔥
+          </span>
+          {따봉}
+        </h5>
+        <p>2월 27일 발행</p>
+      </div>
     </div>
   );
 }
